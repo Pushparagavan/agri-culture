fa7b2edce140d0a30e4c5df3f3a64e269b58b5f1
NadhunYazh <nadhunyazh@NadhunYazhs-MacBook-Air.local>
2024-07-16 14:00:29 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-lime">
-     <h1> Welcome Agriculture </h1>
+     <h1> Welcome To Agriculture </h1>
     </main>
   );
 }
